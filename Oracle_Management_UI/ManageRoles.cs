29365ff90f9b4fd61d386f73125fb013ed0a5aab
ThinhK20 <74862129+ThinhK20@users.noreply.github.com>
2023-03-29 22:30:24 +0700

    Add project files.

---
+++ Oracle_Management_UI/ManageRoles.cs
@@ -0,0 +1,10 @@
+namespace Oracle_Management_UI
+{
+    public partial class ManageRoles : Form
+    {
+        public ManageRoles()
+        {
+            InitializeComponent();
+        }
+    }
+}
