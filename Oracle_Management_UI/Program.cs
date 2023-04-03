b00e20af851356cbbdc3994033b7964e199b5e62
Nguyen Dang Minh <83014500+dangminh71220002@users.noreply.github.com>
2023-04-03 15:23:14 +0700

    Update
    Update

--- Oracle_Management_UI/Program.cs
+++ Oracle_Management_UI/Program.cs
@@ -12,7 +12,7 @@ namespace Oracle_Management_UI
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Oracle_Management_Library.GlobalConfig.InitializeConnections();
-            Application.Run(new EditUser());
+            Application.Run(new ManageRole());
         }
     }
 }
