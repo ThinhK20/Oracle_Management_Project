abfc827bf1cdf5825e4ef169142f5ed3d29466b9
minhnhox792@gmail <minhnhox792@gmail.com>
2023-05-06 22:44:36 +0700

    Merge

--- Oracle_Company/NhanVien.cs
+++ Oracle_Company/NhanVien.cs
@@ -152,9 +152,9 @@
 
         }
 
-		private void textBox6_TextChanged(object sender, EventArgs e)
-		{
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
 
-		}
-	}
+        }
+    }
 }
